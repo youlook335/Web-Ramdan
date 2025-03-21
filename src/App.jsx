0e975a7d13be95dd6c34049f67d47124a6fafb21
unknown <youloos477@gmail.com>
2025-03-21 18:42:50 +0500

    Tailwind css Try

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Home from "./pages/Home";
 import Sehri from "./pages/Sehri";
 import Iftar from "./pages/Iftar";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 const App = () => {
   return (
     <Router>
       <Navbar />
       <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/" element={<Home />} />
         <Route path="/sehri" element={<Sehri />} />
         <Route path="/iftar" element={<Iftar />} />
       </Routes>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}", 
-    "./components/**/*.{js,ts,jsx,tsx}", 
-    "./app/**/*.{js,ts,jsx,tsx}"
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
